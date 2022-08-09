75dc99f629c737590c85a5461446161ab7f4a114
tomz <supaket@enersys.co.th>
2022-08-09 15:08:20 +0700

    Add Ascii

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-		for(String name: context.getBeanDefinitionNames()) {
-			System.out.println(name);
-		}
 	}
 }
